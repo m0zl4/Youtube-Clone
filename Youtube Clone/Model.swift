11d2a7b1e615a30fe00b76ef4e2314e8cda851b0
m0zl4 <zayuka040104@gmail.com>
2021-03-15 11:29:55 +0700

    Day 1 - Setting Youtube API

---
+++ Youtube Clone/Model.swift
@@ -0,0 +1,37 @@
+//
+//  Model.swift
+//  Youtube Clone
+//
+//  Created by muhammad abdul latief on 15/03/21.
+//
+
+import Foundation
+
+class Model {
+  
+  //buat fungsi mgambil data dr youtube API
+  func getVideos() {
+    //nyimpan url ke dlm variabel
+    let url = URL(string: Contsants.API_URL)
+    
+    //kita cek urlnya kosong gk?
+    guard url != nil else {
+      return
+    }
+    
+    //mendapatkan  URLSession dr object
+    let session = URLSession.shared
+    
+    //mendapatkan data dari URLsession
+    let dataTask = session.dataTask(with: url!){ (data, response, error) in
+      
+      //cek kali ada error
+      if  error != nil || data == nil{
+        return
+      }
+    }
+    
+    //start
+    dataTask.resume()
+  }
+}
