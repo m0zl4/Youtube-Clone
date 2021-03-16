f26e7fd779787eb219375e54a41385c19a84bbe2
m0zl4 <zayuka040104@gmail.com>
2021-03-16 11:56:24 +0700

    Days 2 -Parsing JSON / Menstranslate JSON ke model

--- Youtube Clone/Model.swift
+++ Youtube Clone/Model.swift
@@ -29,6 +29,20 @@ class Model {
       if  error != nil || data == nil{
         return
       }
+      
+      do {
+        //memasukan data kedalam file project video
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .iso8601
+        
+        let response = try decoder.decode(Response.self, from: data!)
+        
+        dump(response)
+        
+      }
+      catch {
+        
+      }
     }
     
     //start
